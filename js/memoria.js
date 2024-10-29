b29e9f418bb484dda09b8bab2261cc1360cf1530
Carolina Barrios González <UO275673@uniovi.es>
2024-10-29 14:48:01 +0100

    P6 ejercicio 2 tarea 5 terminada

---
+++ js/memoria.js
@@ -0,0 +1,61 @@
+const elements = {
+    "elements": [
+        {
+            "element": "Redbull",
+            "source":"https://upload.wikimedia.org/wikipedia/de/c/c4/Red_Bull_Racing_logo.svg"
+        },
+        {
+            "element": "Redbull",
+            "source":"https://upload.wikimedia.org/wikipedia/de/c/c4/Red_Bull_Racing_logo.svg"
+        },
+        {
+            "element": "McLaren",
+            "source":"https://upload.wikimedia.org/wikipedia/en/6/66/McLaren_Racing_logo.svg"
+        },
+        {
+            "element": "McLaren",
+            "source":"https://upload.wikimedia.org/wikipedia/en/6/66/McLaren_Racing_logo.svg"
+        },
+        {
+            "element": "Alpine",
+            "source":"https://upload.wikimedia.org/wikipedia/fr/b/b7/Alpine_F1_Team_2021_Logo.svg"
+        },
+        {
+            "element": "Alpine",
+            "source":"https://upload.wikimedia.org/wikipedia/fr/b/b7/Alpine_F1_Team_2021_Logo.svg"
+        },
+        {
+            "element": "AstonMartin",
+            "source":"https://upload.wikimedia.org/wikipedia/fr/7/72/Aston_Martin_Aramco_Cognizant_F1.svg"
+        },
+        {
+            "element": "AstonMartin",
+            "source":"https://upload.wikimedia.org/wikipedia/fr/7/72/Aston_Martin_Aramco_Cognizant_F1.svg"
+        },
+        {
+            "element": "Ferrari",
+            "source":"https://upload.wikimedia.org/wikipedia/de/c/c0/Scuderia_Ferrari_Logo.svg"
+        },
+        {
+            "element": "Ferrari",
+            "source":"https://upload.wikimedia.org/wikipedia/de/c/c0/Scuderia_Ferrari_Logo.svg"
+        },
+        {
+            "element": "Mercedes",
+            "source":"https://upload.wikimedia.org/wikipedia/commons/f/fb/Mercedes_AMG_Petronas_F1_Logo.svg"
+        },
+        {
+            "element": "Mercedes",
+            "source":"https://upload.wikimedia.org/wikipedia/commons/f/fb/Mercedes_AMG_Petronas_F1_Logo.svg"
+        }
+    ]
+};
+
+class Memoria{
+    constructor(){
+        this.hasFlippedCard = false;
+        this.lockBoard = false;
+        this.firstCard = null;
+        this.secondCard = null;
+    }
+}
