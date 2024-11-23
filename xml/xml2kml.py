04e8cceec00c3383b31f511ae4f12f9a871604f8
CarolinaUniovi <uo275673@uniovi.es>
2024-11-23 18:18:29 +0100

    P5 terminada

---
+++ xml/xml2kml.py
@@ -0,0 +1,97 @@
+import xml.etree.ElementTree as ET
+
+def getListaCoord(archivoXML):
+    try:
+        arbol = ET.parse(archivoXML)
+    except IOError:
+        print ('No se encuentra el archivo ', archivoXML)
+        exit()
+
+    except ET.ParseError:
+        print("Error procesando en el archivo XML ", archivoXML)
+        exit()
+
+    raiz = arbol.getroot()
+    lista = []
+
+    #Se extraem las coordenadas
+    for tramo in raiz.findall("{http://www.uniovi.es}tramos/{http://www.uniovi.es}tramo"):
+        print()
+        longuitud = tramo.attrib.get("longitud")
+        latitud = tramo.attrib.get("latitud")
+        altitud = tramo.attrib.get("altitud")
+        lista.append([longuitud, latitud, altitud])
+
+    return lista
+
+def getListaDesc(archivoXML):
+    list = []
+
+    try:
+        tree = ET.parse(archivoXML)
+    except IOError:
+        print('No existe el archivo ' + archivoXML)
+        exit
+
+    except ET.ParseError:
+        print('Error procesando en el archivo .xml ' + archivoXML)
+        exit()
+
+    #Añade el nombre, la localidad y el país
+    root = tree.getroot()
+    list.append(root.find("{http://www.uniovi.es}nombre").text)
+    list.append('Circuito de ' + root.find("{http://www.uniovi.es}localidad").text +
+                ', ' + root.find("{http://www.uniovi.es}pais").text)
+
+    return list
+
+def createKml(listaCoord, desc,output):
+
+    try:
+        file = open("./" + output + ".kml", "w")
+    except FileExistsError:
+        file = open("./" + output + ".kml", "x")
+
+    #Cabecera
+    file.write('<?xml version="1.0" encoding="utf-8"?>\n' +
+                  '<kml xmlns="http://www.opengis.net/kml/2.2">\n')
+
+    file.write('<Document>\n')
+    #Linea de salida
+    file.write('<Placemark>\n' +
+                  '<name>Meta</name>\n' +
+                  '<description>' + desc[0] + '</description>\n' +
+                  '<Point>\n<coordinates>' +
+                  listaCoord[len(listaCoord)-1][0] + ',' +
+                  listaCoord[len(listaCoord)-1][1] + ',' +
+                  listaCoord[len(listaCoord)-1][2] + '</coordinates>\n' +
+                  '</Point>\n</Placemark>\n')
+
+    file.write('<name>'+ desc[0] + '</name>\n')
+
+    file.write('<Placemark>\n' +
+                    '<name>' + desc[0] + '</name>\n' +
+                    '<description>' + desc[1] + '</description>' +
+                    '<LineString>\n<coordinates>\n')
+
+    for coord in listaCoord:
+        file.write(coord[0] + ',' + coord[1] + ',' + coord[2] + '\n')
+
+    file.write(listaCoord[0][0] + ',' + listaCoord[0][1] + ',' + listaCoord[0][2] + '\n')
+
+    file.write('</coordinates>\n</LineString>\n')
+    file.write('<Style>\n<LineStyle>\n<color>#ff0000ff</color>\n' +
+                    '</LineStyle>\n</Style>\n')
+    file.write('</Placemark>\n</Document>\n</kml>')
+    file.close()
+    print("Archivo generado correctamente")
+
+def main():
+    file = "circuitoEsquema.xml"
+    listaCoord = getListaCoord(file)
+    listaDesc = getListaDesc(file)
+    createKml(listaCoord,listaDesc,"circuito")
+
+
+if __name__ == "__main__":
+    main()
---
+++ xml/xml2perfil.py
@@ -0,0 +1,60 @@
+import xml.etree.ElementTree as ET
+
+def getListaAltitud(archivoXML):
+    try:
+        tree = ET.parse(archivoXML)
+    except IOError:
+        print('No existe el archivo ' + archivoXML)
+        exit
+
+    except ET.ParseError:
+        print('Error procesando en el archivo .xml ' + archivoXML)
+        exit()
+
+    raiz = tree.getroot()
+    lista = []
+
+    for tramo in raiz.findall("{http://www.uniovi.es}tramos/{http://www.uniovi.es}tramo"):
+        nsector = tramo.attrib.get("sector")
+        altitud = tramo.attrib.get("altitud")
+        lista.append([nsector, altitud])
+
+    return lista
+
+def createSvgPerfil(archivoXML, output):
+    try:
+        svgFile = open("./" + output + ".svg", "w")
+    except FileExistsError:
+        svgFile = open("./" + output + ".svg", "x")
+
+    svgFile.write('<?xml version="1.0" encoding="utf-8"?>\n' +
+                  '<svg xmlns="http://www.w3.org/2000/svg" version="2.0">\n')
+    svgFile.write('<polyline points = "\n')
+
+    base = 300
+    init = 60
+    separacion = 20
+
+    svgFile.write(str(init) + ', ' + str(base) + '\n')
+
+    incr = 0
+    for element in archivoXML:
+        incr += 1
+        svgFile.write(str(separacion*incr+40) + ',' + str(base-float(element[1])) + '\n')
+
+    svgFile.write((str(separacion*incr+40)) + ', ' + str(base) + '\n')
+    svgFile.write(str(init-1.5) + ', ' + str(base) + '\n')
+
+    svgFile.write('"\nstyle="fill:white;stroke:orange;stroke-width:4" />\n')
+
+    svgFile.write('</svg>')
+    print("Archivo generado correctamente")
+
+
+
+def main():
+    createSvgPerfil(getListaAltitud("circuitoEsquema.xml"),
+                    "altimetria")
+
+if __name__ == "__main__":
+    main()
