b2946aba4761bb8805290361904d8c7e1b775f4c
CarolinaUniovi <uo275673@uniovi.es>
2024-11-25 18:09:45 +0100

    P7 ejercicio 2 terminado

--- js/pais.js
+++ js/pais.js
@@ -50,6 +50,7 @@ class Pais {
     //Haz que el servicio web devuelva la información del tiempo en formato XML, en idioma español y con unidades de medida del sistema métrico.
     apikey = "4e7ac9eaf132fd8c0117221734ed2258";
     getMeteoData() {
+        var html = "";
         var url = "https://api.openweathermap.org/data/2.5/forecast?lat=" + this.coordMeta.slice(0, 8) + "&lon=" + this.coordMeta.slice(10)+"&appid=" + this.apikey + "&units=metric&mode=xml&lang=es";
         $.ajax({
             url: url,
@@ -57,18 +58,52 @@ class Pais {
             method: 'GET',
             success: function (datos) {
                 console.log(datos);
-                $("h5").text((new XMLSerializer()).serializeToString(datos));
-                var time =  $('time:first', datos);
-                var date =  $('time:first', datos).attr("from").slice(0, 10);
                 var forecast =  $('forecast time', datos);
-                console.log(time);
-                
+                var day = -1;
+                var lastDateI = 0;
+                var maxTemp = 0;
+                var minTemp = 0;
+                var humidity = 0;
+                var lastDate = " ";
                 for(let i = 0; i<forecast.length; i++){
-                    var nextTime = $('time:eq(' + i + ')', datos);
                     var newDate = $('time:eq(' + i + ')', datos).attr("from").slice(0, 10);
-                    console.log(newDate);
-                    
+                    var temp = parseFloat($('temperature:eq(' + i + ')', datos).attr("value"));
+                    if(lastDate == newDate){
+                        humidity += parseFloat($('humidity:eq(' + i + ')', datos).attr("value"));
+                        if(maxTemp < temp){
+                            maxTemp  = temp;
+                        } else if(minTemp > temp){
+                            minTemp = temp;
+                        }
+                    } else {
+                        day++;
+                        if(day > 0){
+                            
+                            humidity = humidity/(i- lastDateI);
+                            html += "<article>";
+                            html += "<h4>" + lastDate + "</h4>";
+                            html += "<img src='https://openweathermap.org/img/wn/" + $('symbol:eq(' + parseInt(i-(i- lastDateI)/2) + ')', datos).attr("var")  +"@2x.png'/>";
+                            html += "<p>Máxima:" + maxTemp +"ºC</p>";
+                            html += "<p>Mínima:" + minTemp +"ºC</p>";
+                            html += "</article>";
+                            lastDateI = i;
+                        }
+                        lastDate = newDate;
+                        minTemp = temp;
+                        maxTemp = temp;
+                        humidity = parseFloat($('humidity:eq(' + i + ')', datos).attr("value"));
+                    }     
                 }
+                humidity = humidity/(forecast.length- lastDateI);
+                html += "<article>";
+                html += "<h4>" + lastDate + "</h4>";
+                html += "<img src='https://openweathermap.org/img/wn/" + $('symbol:eq(' + parseInt(forecast.length-(forecast.length- lastDateI)/2) + ')', datos).attr("var")  +"@2x.png'/>";
+                html += "<p>Máxima:" + maxTemp +"ºC</p>";
+                html += "<p>Mínima:" + minTemp +"ºC</p>";
+                html += "</article>";
+                $("section").append(html);
+                
+              
                 
             },
             error: function (err) {
--- xml/xml2kml.py
+++ xml/xml2kml.py
@@ -52,7 +52,6 @@ def createKml(listaCoord, desc,output):
     except FileExistsError:
         file = open("./" + output + ".kml", "x")
 
-    #Cabecera
     file.write('<?xml version="1.0" encoding="utf-8"?>\n' +
                   '<kml xmlns="http://www.opengis.net/kml/2.2">\n')
 
