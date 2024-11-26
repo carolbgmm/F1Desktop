f4252ebe22a6898fc31a9afb02d52bda31ff00a1
Carolina Barrios González <UO275673@uniovi.es>
2024-11-26 14:50:19 +0100

    P6 ejercicio 3 avances tarea 5

--- js/semaforo.js
+++ js/semaforo.js
@@ -13,28 +13,27 @@ class Semaforo{
     }
 
     createStructure(){
-        var section = document.createElement("section");
+        var section = document.getElementById("sectionSemaforo");
         var h2 = document.createElement("h2");
         h2.appendChild(document.createTextNode("Semáforo"));
-        section.appendChild(h2);
+        
         var section2 = document.createElement("section");
         for(let i = 0; i<this.ligths; i++){
-            section2.appendChild(document.createElement("div"));
+            var div = document.createElement("div");
+            div.id = "div" + i;
+            section2.appendChild(div);
         }
-        section.appendChild(section2);
-        var section3 = document.createElement("section");
-        var startBtn = document.createElement("button");
-        var reactBtn = document.createElement("button");
-        startBtn.appendChild(document.createTextNode("Start"));
-        reactBtn.appendChild(document.createTextNode("Tiempo de reacción"));
-        section3.appendChild(startBtn);
-        section3.appendChild(reactBtn);
-        section.appendChild(section3);
-        document.body.appendChild(section);
+        var startBtn = document.getElementById("btnArranque");
+        var reactBtn = document.getElementById("btnReaccion");
+        startBtn.appendChild(document.createTextNode("Arranque"));
+        reactBtn.appendChild(document.createTextNode("Reacción"));
+        section.prepend(section2);
+        section.prepend(h2)
+
     }
 
     initSequence(){
-
+        document.getElementsById("div0");
     }
 
 }
